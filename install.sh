6d3a1973bf395fc7a47977c38512ffed9d6ed543
Sadab72009 <shaikhsadab72009@gmail.com>
2024-09-05 10:18:38 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
+
 mkdir -p .devcontainer
+
 cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/Demo/soul.py",
+    "features": {
+        "ghcr.io/devcontainers/features/sshd:1": {
+            "version": "latest"
+        }
+    },
+    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/soul.py",
     "customizations": {
         "vscode": {
             "settings": {
@@ -18,5 +25,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 EOL
 
 git add .devcontainer/devcontainer.json
-git commit -m "Add postStartCommand to run Python script automatically"
+
+git commit -m "Add postStartCommand to run Python script automatically with repo name detection"
+
 git push origin main
